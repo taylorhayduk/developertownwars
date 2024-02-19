28d05efe3a24378dedf1bb4a52e56d9981eb00f5
Taylor Hayduk <taylorhayduk@gmail.com>
2024-02-19 10:55:01 -0500

    fix ts issue with deploy

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -4,11 +4,25 @@ import styles from "./Button.module.css";
 interface ButtonProps {
   onClick: () => void;
   children: React.ReactNode;
+  status?: {
+    pending: boolean;
+  };
+  type?: "button" | "submit" | "reset";
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
+const Button: React.FC<ButtonProps> = ({
+  onClick,
+  children,
+  status,
+  type = "button",
+}) => {
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button
+      className={styles.button}
+      onClick={onClick}
+      aria-disabled={status?.pending}
+      type={type}
+    >
       {children}
     </button>
   );
